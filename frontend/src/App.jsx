2d9088c83759a49a86e948121f3418ef35f490ee
Abdurraheem2431 <abdurraheem1503@gmail.com>
2026-05-16 19:27:45 -0400

    fix main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import InputPage from './components/InputPage'
 import ResultsPage from './components/ResultsPage'
-import './App.css'
+import './css/App.css'
 
 function App() {
   return (
